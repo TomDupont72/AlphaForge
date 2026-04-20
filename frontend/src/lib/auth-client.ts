c6f2f8317ea94d84c0d7450ff58e37379b6b2bec
Dupont Tom <tomdupont72@gmail.com>
2026-04-20 23:45:45 +0200

    Docker donc + CD

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,7 +1,6 @@
 import { createAuthClient } from "better-auth/client";
 import { environment } from '@/../environments/environments';
 
-
 export const authClient = createAuthClient({
   baseURL: environment.apiUrl
 });
